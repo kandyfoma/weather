b43bb961956deba5e73f99b51d19e6d121f10e40
fomakyala <kandy@siege.ai>
2020-11-19 14:57:01 +0200

    styling

--- openweather/views.py
+++ openweather/views.py
@@ -1,7 +1,6 @@
-from django.http import HttpResponse
 from django.shortcuts import render
-from django.views.generic import TemplateView
-import requests, json
+import requests
+from django.http import JsonResponse
 
 
 def index(request):
@@ -14,6 +13,12 @@ def weather_request(request):
 
     city = request.POST.get('city')
     period = request.POST.get('period')
+    temp_min = []
+    temp_max = []
+    temp = []
+    humidity = []
+    tem_median = []
+    days = [x for x in range(1, int(period) + 1)]
 
     URL = BASE_URL + "q=" + city + "&units=metric&cnt=" + period + "&lang=en" + "&appid=" + API_KEY
 
@@ -24,27 +29,32 @@ def weather_request(request):
         data = response.json()
         # getting the main dict block
         list_weather = data['list']
-        tem_min = []
-        tem_max = []
-        temp = []
-        humidity = []
-        tem_median = []
+
         for i in range(len(list_weather)):
             temp.append(list_weather[i]['main']['temp'])
-            tem_min.append(list_weather[i]['main']['temp_min'])
-            tem_max.append(list_weather[i]['main']['temp_max'])
+            temp_min.append(list_weather[i]['main']['temp_min'])
+            temp_max.append(list_weather[i]['main']['temp_max'])
             humidity.append(list_weather[i]['main']['humidity'])
             median = (list_weather[i]['main']['temp_min'] + list_weather[i]['main']['temp_max']) / 2
             tem_median.append(median)
         context = {
             'temp': temp,
-            'tem_min': tem_min,
-            'tem_max': tem_max,
+            'tem_min': temp_min,
+            'tem_max': temp_max,
             'tem_median': tem_median,
             'humidity': humidity,
+            'days': days
         }
     else:
         # showing the error message
         print("Error in the HTTP request")
 
-    return render(request, 'index.html', context=context)
+    return render(request, 'index.html', {
+        'labels': days,
+        'data_temp': temp,
+        'data_max_temp': temp_max,
+        'data_min_temp': temp_min,
+        'data_humidity': humidity,
+        'data_tem_median': tem_median,
+
+    })
